3da0599bc78abcc7e3657e87f9197e767af61a8d
DonChaka <DonChaka98@gmail.com>
2020-11-26 16:31:41 +0100

    extended data.sql on frontend request

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -4,7 +4,18 @@ INSERT INTO app_user(email, first_name, last_name, password) VALUES
 ('c.ebula@gmail.com', 'Celina', 'Ebula', '$2a$10$UMHEGIuKed.l0tTbZDGExe0Q6lywnCC0VMgjbOBa9H2WuDKbpwPuq'),
 ('j.wick@gmail.com', 'John', 'Wick', '$2a$10$UMHEGIuKed.l0tTbZDGExe0Q6lywnCC0VMgjbOBa9H2WuDKbpwPuq'),
 ('m.wazowski@gmail.com', 'Mike', 'Wazowski', '$2a$10$UMHEGIuKed.l0tTbZDGExe0Q6lywnCC0VMgjbOBa9H2WuDKbpwPuq'),
-('s.sullivan@gmail.com', 'Sully', 'Sulivan', '$2a$10$UMHEGIuKed.l0tTbZDGExe0Q6lywnCC0VMgjbOBa9H2WuDKbpwPuq');
+('s.sullivan@gmail.com', 'Sully', 'Sulivan', '$2a$10$UMHEGIuKed.l0tTbZDGExe0Q6lywnCC0VMgjbOBa9H2WuDKbpwPuq'),
+('qq@gmail.com', 'q', 'q', '$2a$10$UMHEGIuKed.l0tTbZDGExe0Q6lywnCC0VMgjbOBa9H2WuDKbpwPuq'),
+('ww@gmail.com', 'w', 'w', '$2a$10$UMHEGIuKed.l0tTbZDGExe0Q6lywnCC0VMgjbOBa9H2WuDKbpwPuq'),
+('ee@gmail.com', 'e', 'e', '$2a$10$UMHEGIuKed.l0tTbZDGExe0Q6lywnCC0VMgjbOBa9H2WuDKbpwPuq'),
+('rr@gmail.com', 'r', 'r', '$2a$10$UMHEGIuKed.l0tTbZDGExe0Q6lywnCC0VMgjbOBa9H2WuDKbpwPuq'),
+('tt@gmail.com', 't', 't', '$2a$10$UMHEGIuKed.l0tTbZDGExe0Q6lywnCC0VMgjbOBa9H2WuDKbpwPuq'),
+('yy@gmail.com', 'y', 'y', '$2a$10$UMHEGIuKed.l0tTbZDGExe0Q6lywnCC0VMgjbOBa9H2WuDKbpwPuq'),
+('uu@gmail.com', 'u', 'u', '$2a$10$UMHEGIuKed.l0tTbZDGExe0Q6lywnCC0VMgjbOBa9H2WuDKbpwPuq'),
+('ii@gmail.com', 'i', 'i', '$2a$10$UMHEGIuKed.l0tTbZDGExe0Q6lywnCC0VMgjbOBa9H2WuDKbpwPuq'),
+('oo@gmail.com', 'o', 'o', '$2a$10$UMHEGIuKed.l0tTbZDGExe0Q6lywnCC0VMgjbOBa9H2WuDKbpwPuq'),
+('pp@gmail.com', 'p', 'p', '$2a$10$UMHEGIuKed.l0tTbZDGExe0Q6lywnCC0VMgjbOBa9H2WuDKbpwPuq');
+
 
 INSERT INTO app_group(description, guid, name) VALUES
 ('Grupa testowa A', 'AAAAAA', 'A Grupa'),
@@ -30,5 +41,14 @@ INSERT INTO user_group(google_calendar, role, group_id, user_id) VALUES
 (false, 1, 2, 5),
 (false, 2, 3, 5),
 (false, 1, 4, 5),
-(false, 3, 6, 5);
-
+(false, 3, 6, 5),
+(false, 3, 5, 7),
+(false, 3, 5, 8),
+(false, 3, 5, 9),
+(false, 3, 5, 10),
+(false, 3, 5, 11),
+(false, 3, 5, 12),
+(false, 3, 5, 13),
+(false, 3, 5, 14),
+(false, 3, 5, 15),
+(false, 3, 5, 16);
