bbe202909dd942d5d48ff6f102aa51c8ec24bb1b
DonChaka <DonChaka98@gmail.com>
2020-11-26 10:49:31 +0100

    Added endpoints to update, delete and get logged user

--- src/main/java/pl/politechnika/goalreacher/controller/UserController.java
+++ src/main/java/pl/politechnika/goalreacher/controller/UserController.java
@@ -1,23 +1,21 @@
 package pl.politechnika.goalreacher.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import pl.politechnika.goalreacher.entity.AppUser;
 import pl.politechnika.goalreacher.service.UserService;
 
 @Controller
 @RequestMapping("/users")
-public class UserController {
+public class UserController
+{
     private final UserService userService;
 
-    @Autowired
-    public UserController(UserService userService) {
+    public UserController(UserService userService)
+    {
         this.userService = userService;
     }
 
@@ -25,6 +23,31 @@ public class UserController {
     public ResponseEntity<Iterable<AppUser>> getAll()
     {
         return new ResponseEntity<>(userService.findAllUsers(), HttpStatus.OK);
+    }
+
+    @GetMapping()
+    ResponseEntity<AppUser> getUser(Authentication authentication)
+    {
+        try
+        {
+            return new ResponseEntity<>(userService.getLoggedUser(authentication), HttpStatus.OK);
+        } catch (Exception e)
+        {
+            return new ResponseEntity<>(HttpStatus.FORBIDDEN);
+        }
+    }
+
+    @DeleteMapping
+    ResponseEntity<AppUser> deleteUser(Authentication authentication)
+    {
+        try
+        {
+            userService.deleteUser(authentication);
+            return new ResponseEntity<>(HttpStatus.OK);
+        } catch (Exception e)
+        {
+            return new ResponseEntity<>(HttpStatus.FORBIDDEN);
+        }
     }
 
     @PostMapping()
@@ -35,5 +58,18 @@ public class UserController {
         if (check == null)
             return new ResponseEntity<>(userService.saveUser(newUser), HttpStatus.CREATED);
         return new ResponseEntity<>(HttpStatus.UNPROCESSABLE_ENTITY);
+    }
+
+    @PutMapping()
+    ResponseEntity<AppUser> updateUser(@RequestBody AppUser updatedUser, Authentication authentication)
+    {
+        try
+        {
+            return new ResponseEntity<>(userService.updateUser(updatedUser, authentication), HttpStatus.OK);
+        } catch (Exception e)
+        {
+            return new ResponseEntity<>(HttpStatus.UNPROCESSABLE_ENTITY);
+        }
+
     }
 }
--- src/main/java/pl/politechnika/goalreacher/service/UserService.java
+++ src/main/java/pl/politechnika/goalreacher/service/UserService.java
@@ -1,20 +1,28 @@
 package pl.politechnika.goalreacher.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import pl.politechnika.goalreacher.Exceptions.EmailTakenException;
+import pl.politechnika.goalreacher.Exceptions.NotAuthorizedException;
+import pl.politechnika.goalreacher.Exceptions.UserNotExistingException;
 import pl.politechnika.goalreacher.entity.AppUser;
+import pl.politechnika.goalreacher.entity.UserGroup;
+import pl.politechnika.goalreacher.repository.UserGroupRepository;
 import pl.politechnika.goalreacher.repository.UserRepository;
 
 @Service
-public class UserService {
+public class UserService
+{
 
     private final UserRepository userRepository;
+    private final UserGroupRepository userGroupRepository;
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
-    @Autowired
-    public UserService(UserRepository userRepository, BCryptPasswordEncoder bCryptPasswordEncoder) {
+    public UserService(UserRepository userRepository, UserGroupRepository userGroupRepository, BCryptPasswordEncoder bCryptPasswordEncoder)
+    {
         this.userRepository = userRepository;
+        this.userGroupRepository = userGroupRepository;
         this.bCryptPasswordEncoder = bCryptPasswordEncoder;
     }
 
@@ -24,14 +32,60 @@ public class UserService {
         return userRepository.save(newUser);
     }
 
+    public AppUser updateUser(AppUser updatedUser, Authentication authentication) throws Exception
+    {
+        AppUser authenticatedUser = userRepository.findByEmail(authentication.getPrincipal().toString());
+        if (authenticatedUser == null)
+            throw new UserNotExistingException();
+
+        if (updatedUser.getFirstName() != null && !updatedUser.getFirstName().equals(""))
+            authenticatedUser.setFirstName(updatedUser.getFirstName());
+        if (updatedUser.getLastName() != null && !updatedUser.getLastName().equals(""))
+            authenticatedUser.setLastName(updatedUser.getLastName());
+        if (updatedUser.getPassword() != null && !updatedUser.getPassword().equals(""))
+            authenticatedUser.setPassword(bCryptPasswordEncoder.encode(updatedUser.getPassword()));
+        if (updatedUser.getEmail() != null && !updatedUser.getEmail().equals(""))
+        {
+            AppUser emailTestUser = userRepository.findByEmail(updatedUser.getEmail());
+            if (emailTestUser == null || emailTestUser.equals(authenticatedUser))
+                authenticatedUser.setEmail(updatedUser.getEmail());
+            else throw new EmailTakenException();
+        }
+
+        userRepository.save(authenticatedUser);
+        return userRepository.save(authenticatedUser);
+    }
+
     public Iterable<AppUser> findAllUsers()
     {
         return userRepository.findAll();
     }
 
-    public AppUser findByEmail(String email) {
+    public AppUser findByEmail(String email)
+    {
         return userRepository.findByEmail(email);
     }
 
 
+    public AppUser getLoggedUser(Authentication authentication) throws Exception
+    {
+        AppUser logged = userRepository.findByEmail(authentication.getPrincipal().toString());
+        if (logged == null)
+            throw new UserNotExistingException();
+        return logged;
+
+    }
+
+    public void deleteUser(Authentication authentication) throws Exception
+    {
+        AppUser toDel = userRepository.findByEmail(authentication.getPrincipal().toString());
+        if (toDel == null) throw new NotAuthorizedException();
+
+        for (UserGroup group : toDel.getGroups())
+        {
+            userGroupRepository.delete(group);
+        }
+
+        userRepository.delete(toDel);
+    }
 }
