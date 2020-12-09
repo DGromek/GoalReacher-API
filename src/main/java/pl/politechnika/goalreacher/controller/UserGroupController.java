f9c3ba8618f36ad40a71a2b220f9d0650d5bf72e
DGromek <dawidgromek.1997@gmail.com>
2020-12-09 19:28:22 +0100

    Gods have forsaken this code v2

--- src/main/java/pl/politechnika/goalreacher/controller/UserGroupController.java
+++ src/main/java/pl/politechnika/goalreacher/controller/UserGroupController.java
@@ -15,19 +15,16 @@ import pl.politechnika.goalreacher.service.UserGroupService;
 
 @Controller
 @RequestMapping()
-public class UserGroupController
-{
+public class UserGroupController {
     private final UserGroupService userGroupRepository;
 
     @Autowired
-    public UserGroupController(UserGroupService userGroupRepository)
-    {
+    public UserGroupController(UserGroupService userGroupRepository) {
         this.userGroupRepository = userGroupRepository;
     }
 
     @PostMapping("/joinGroup")
-    public ResponseEntity<AppUser> joinGroup(@RequestBody JoinGroupDTO joinGroupDTO)
-    {
+    public ResponseEntity<AppUser> joinGroup(@RequestBody JoinGroupDTO joinGroupDTO) {
         UserGroup joined = userGroupRepository.joinGroup(joinGroupDTO);
 
         if (joined == null)
@@ -37,43 +34,31 @@ public class UserGroupController
     }
 
     @PutMapping("/users/changeStatus")
-    public ResponseEntity<UserGroup> changeUserStatus(@RequestBody ChangeStatusDTO changeStatusDTO, Authentication authentication)
-    {
-        try
-        {
-            UserGroup toChange = userGroupRepository.changeStatus(changeStatusDTO, authentication);
-            return new ResponseEntity<>(toChange, HttpStatus.OK);
-        } catch (Exception e)
-        {
+    public ResponseEntity<UserGroup> changeUserStatus(@RequestBody ChangeStatusDTO changeStatusDTO, Authentication authentication) {
+        UserGroup toChange = userGroupRepository.changeStatus(changeStatusDTO, authentication.getPrincipal().toString());
+        if (toChange == null) {
             return new ResponseEntity<>(HttpStatus.UNPROCESSABLE_ENTITY);
         }
+        return new ResponseEntity<>(toChange, HttpStatus.OK);
     }
 
     @DeleteMapping("users/leaveGroup")
-    public ResponseEntity<AppUser> leaveGroup(@RequestBody ChangeStatusDTO changeStatusDTO, Authentication authentication)
-    {
-        try
-        {
+    public ResponseEntity<AppUser> leaveGroup(@RequestBody ChangeStatusDTO changeStatusDTO, Authentication authentication) {
+        try {
             userGroupRepository.leaveGroup(changeStatusDTO, authentication);
             return new ResponseEntity<>(HttpStatus.OK);
-        } catch (Exception e)
-        {
+        } catch (Exception e) {
             return new ResponseEntity<>(HttpStatus.UNPROCESSABLE_ENTITY);
         }
     }
 
     @PostMapping("/joinFromInvitation")
-    public ResponseEntity<UserGroup> joinFromInvitation(@RequestParam long invitationId, Authentication authentication)
-    {
-        try
-        {
+    public ResponseEntity<UserGroup> joinFromInvitation(@RequestParam long invitationId, Authentication authentication) {
+        try {
             return new ResponseEntity<>(userGroupRepository.joinFromInvitation(invitationId, authentication), HttpStatus.OK);
-        } catch (NotAuthorizedException e)
-        {
+        } catch (NotAuthorizedException e) {
             return new ResponseEntity<>(HttpStatus.FORBIDDEN);
-        }
-        catch (Exception e)
-        {
+        } catch (Exception e) {
             return new ResponseEntity<>(HttpStatus.UNPROCESSABLE_ENTITY);
         }
 
--- src/main/java/pl/politechnika/goalreacher/service/UserGroupService.java
+++ src/main/java/pl/politechnika/goalreacher/service/UserGroupService.java
@@ -19,34 +19,31 @@ import pl.politechnika.goalreacher.repository.UserRepository;
 import java.util.Optional;
 
 @Service
-public class UserGroupService
-{
+public class UserGroupService {
     private final UserGroupRepository userGroupRepository;
     private final UserRepository userRepository;
     private final GroupRepository groupRepository;
     private final InvitationRepository invitationRepository;
 
     @Autowired
-    public UserGroupService(UserGroupRepository userGroupRepository, UserRepository userRepository, GroupRepository groupRepository, InvitationRepository invitationRepository)
-    {
+    public UserGroupService(UserGroupRepository userGroupRepository, UserRepository userRepository, GroupRepository groupRepository, InvitationRepository invitationRepository) {
         this.userGroupRepository = userGroupRepository;
         this.userRepository = userRepository;
         this.groupRepository = groupRepository;
         this.invitationRepository = invitationRepository;
     }
 
-    public UserGroup joinGroup(JoinGroupDTO joinGroupDTO)
-    {
+    public UserGroup joinGroup(JoinGroupDTO joinGroupDTO) {
         AppGroup joiningGroup = groupRepository.findByGuid(joinGroupDTO.getTargetGroupGuid());
-        if (joiningGroup == null)
+        if (joiningGroup == null) {
             return null;
+        }
 
         Optional<AppUser> joiningUser = userRepository.findById(joinGroupDTO.getUserId());
         if (!joiningUser.isPresent())
             return null;
 
-        for (UserGroup userGroup : joiningGroup.getUsers())
-        {
+        for (UserGroup userGroup : joiningGroup.getUsers()) {
             if (userGroup.getUser().equals(joiningUser.get()))
                 return null;
         }
@@ -59,62 +56,52 @@ public class UserGroupService
         return userGroupRepository.save(newUserGroup);
     }
 
-    private UserGroup getUserGroup(String email, String guid) throws Exception
-    {
+    private UserGroup getUserGroup(String email, String guid) {
         AppGroup appGroup = groupRepository.findByGuid(guid);
         AppUser appUser = userRepository.findByEmail(email);
-        if (appUser == null)
-            throw new UserNotExistingException();
-        if (appGroup == null)
-            throw new GroupNotExistingException();
+
         return userGroupRepository.findByUserAndGroup(appUser, appGroup);
     }
 
-    private boolean canOverrideStatus(UserGroup changee, UserGroup changer, Role role2B)
-    {
-        if(role2B != null && role2B.ordinal() == 0)
+    private boolean canOverrideStatus(UserGroup changee, UserGroup changer, Role role2B) {
+        if (role2B != null && role2B.ordinal() == 0) {
             return false;
-        if(changee.getGroup().getId() == changer.getGroup().getId())
-        {
-            if(changer.getRole().ordinal() == 0 && role2B.ordinal() > 0)
+        }
+        if (changee.getGroup().getId() == changer.getGroup().getId()) {
+            if (changer.getRole().ordinal() == 0 && role2B.ordinal() > 0) {
                 return true;
+            }
 
             return changer.getRole().ordinal() == 1 && changer.getRole().ordinal() < changee.getRole().ordinal();
         }
         return false;
     }
 
-    public UserGroup changeStatus(ChangeStatusDTO changeStatusDTO, Authentication authentication) throws Exception
-    {
+    public UserGroup changeStatus(ChangeStatusDTO changeStatusDTO, String email) {
         UserGroup toChange = getUserGroup(changeStatusDTO.getTargetUserEmail(), changeStatusDTO.getTargetGroupGuid());
-        UserGroup changer = getUserGroup(authentication.getPrincipal().toString(), changeStatusDTO.getTargetGroupGuid());
-        if(toChange == null || changer == null)
-            throw new UserNotInGroupException();
+        UserGroup changer = getUserGroup(email, changeStatusDTO.getTargetGroupGuid());
+        if (toChange == null || changer == null)
+            return null;
 
-        if(canOverrideStatus(toChange, changer, changeStatusDTO.getNewRole()))
+        if (canOverrideStatus(toChange, changer, changeStatusDTO.getNewRole())) {
             toChange.setRole(changeStatusDTO.getNewRole());
-        else
-            throw new NotAuthorizedException();
-
-        return userGroupRepository.save(toChange);
+            return userGroupRepository.save(toChange);
+        }
+        return null;
     }
 
-    public void leaveGroup(ChangeStatusDTO changeStatusDTO, Authentication authentication) throws Exception
-    {
+    public void leaveGroup(ChangeStatusDTO changeStatusDTO, Authentication authentication) throws Exception {
         UserGroup toChange = getUserGroup(changeStatusDTO.getTargetUserEmail(), changeStatusDTO.getTargetGroupGuid());
         UserGroup changer = getUserGroup(authentication.getPrincipal().toString(), changeStatusDTO.getTargetGroupGuid());
-        if(toChange == null || changer == null)
+        if (toChange == null || changer == null) {
             throw new UserNotInGroupException();
+        }
 
-        if(changeStatusDTO.getTargetUserEmail().equals(authentication.getPrincipal().toString()))
-        {
+        if (changeStatusDTO.getTargetUserEmail().equals(authentication.getPrincipal().toString())) {
             userGroupRepository.delete(toChange);
             return;
-        }
-        else
-        {
-            if(changer.getRole() == Role.CREATOR || (changer.getRole() == Role.ADMIN && toChange.getRole().ordinal() > 1))
-            {
+        } else {
+            if (changer.getRole() == Role.CREATOR || (changer.getRole() == Role.ADMIN && toChange.getRole().ordinal() > 1)) {
                 userGroupRepository.delete(toChange);
                 return;
             }
@@ -122,24 +109,22 @@ public class UserGroupService
         throw new NotAuthorizedException();
     }
 
-    public UserGroup joinFromInvitation(long invitationId, Authentication authentication) throws Exception
-    {
+    public UserGroup joinFromInvitation(long invitationId, Authentication authentication) throws Exception {
         Optional<Invitation> invitation = invitationRepository.findById(invitationId);
-        if(!invitation.isPresent()) throw new InvitationNotExistingException();
+        if (!invitation.isPresent()) throw new InvitationNotExistingException();
 
         Optional<AppGroup> group = groupRepository.findById(invitation.get().getGroup().getId());
-        if(!group.isPresent()) throw new GroupNotExistingException();
+        if (!group.isPresent()) throw new GroupNotExistingException();
         Optional<AppUser> invited = userRepository.findById(invitation.get().getInvited().getId());
-        if(!invited.isPresent()) throw new UserNotExistingException();
+        if (!invited.isPresent()) throw new UserNotExistingException();
         AppUser authenticated = userRepository.findByEmail(authentication.getPrincipal().toString());
-        if(authenticated != invited.get()) throw new NotAuthorizedException();
+        if (authenticated != invited.get()) throw new NotAuthorizedException();
 
         UserGroup userGroup = userGroupRepository.findByUserAndGroup(invited.get(), group.get());
-        if(userGroup != null)
-        {
-            if(userGroup.getRole() != Role.PENDING) throw new UserAlreadyInGroupException();
-            else
-            {
+        if (userGroup != null) {
+            if (userGroup.getRole() != Role.PENDING) {
+                throw new UserAlreadyInGroupException();
+            } else {
                 userGroup.setRole(Role.USER);
                 return userGroupRepository.save(userGroup);
             }
